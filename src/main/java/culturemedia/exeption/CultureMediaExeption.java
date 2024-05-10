c3cd04742236b82ad34f118c279204bcbaf7a032
5h4rks <1211rogerandres2015@gmail.com>
2024-05-10 17:31:44 -0500

    v 1.4

---
+++ src/main/java/culturemedia/exeption/CultureMediaExeption.java
@@ -0,0 +1,8 @@
+package main.java.culturemedia.exeption;
+public class CultureMediaExeption extends Exception {
+    
+    public CultureMediaExeption(String messege){
+        super(messege);
+        
+    }
+}
